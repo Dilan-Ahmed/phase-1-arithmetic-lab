ac00bd96e2d3d45f47cd858993eec165b74a9df1
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-02-17 20:51:00 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-
 // Write your code here
+
+// 1st task
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+// 2nd task
+let random = Math.floor(Math.random() * 10);
+console.log(random);
+// 3rd task
+const num3 = 20;
+const num4 = 8;
+
+const mod = num3 % num4;
+console.log(mod);
+// 4th task
+
+let max = Math.max(1, 2, 4, 8, 12, 20);
+
+console.log(max);
